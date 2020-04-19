d3e3aa4a581064f9cfd8fad751dd77422046a8f1
bermyllerazon <bermylle@gmail.com>
2020-04-19 17:11:40 +0800

    fixed cases

--- app.py
+++ app.py
@@ -13,17 +13,19 @@ covid = Covid()
 # get ph data
 ph_cases = covid.get_status_by_country_name("philippines")
 
-filtered_cases = { 'confirmed' : ph_cases.get("confirmed"),
-'active' : ph_cases.get("active"), 'recovered' : ph_cases.get("recovered"), 'deaths' :  ph_cases.get("deaths") }
-print(filtered_cases)
+# filtered_cases = { 'confirmed' : ph_cases.get("confirmed"),
+# 'active' : ph_cases.get("active"), 'recovered' : ph_cases.get("recovered"), 'deaths' :  ph_cases.get("deaths") }
+
+confirmed_case = {'confirmed' : ph_cases.get("confirmed")}
+active_case = {'active' : ph_cases.get("active")}
+recovered_case = {'recovered' : ph_cases.get("recovered")}
+death_case = {'deaths' :  ph_cases.get("deaths")}
 
 
-
-print(type(ph_cases))
 # MAIN
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-	return render_template('index.html', cases = filtered_cases)
+	return render_template('index.html', confirmed = confirmed_case, active = active_case, recovered = recovered_case, death = death_case)
 
 @app.route('/ph')
 def ph():
