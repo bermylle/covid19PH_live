4525f4b571852fd51d338d32c4b5366e0ef79ac7
bermylle <50631121+bermylle@users.noreply.github.com>
2020-04-22 20:02:00 +0800

    first commit:

--- app.py
+++ app.py
@@ -40,4 +40,6 @@ def contact():
 def about():
 	return render_template('about.html')
 
-app.run()
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
